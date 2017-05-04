f523aec02ed0f294366d41b092774bff2ea5df1c
luispuchades <luispuchades@live.com>
2017-05-04 07:00:29 +0200

    UDACITY_ANDROIDFootballMuch
    UDACITY Android Begginners Path - Lesson2 - SCORE KEEPER APP

--- app/src/main/java/com/example/android/footballmuch/MainActivity.java
+++ app/src/main/java/com/example/android/footballmuch/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity {
 
     //Variables declaration and initialization
@@ -36,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
     //Event declaration
     //*****************
-
     public void addGoalTeamA(View view) {
         goalsTeamA = goalsTeamA + 1;
         displayGoalsTeamA(goalsTeamA);
